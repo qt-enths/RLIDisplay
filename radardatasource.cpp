38b776d24d22b22e8ccca4c832a2924ff1ceeba8
mamikhajjlov <mamikhajjlov@ipmce.ru>
2016-08-08 10:34:12 +0300

    Radar data (file_amps) are loaded with constant amplitudes (64 and 128) to debug "moire" pattern in RLI display

--- radardatasource.cpp
+++ radardatasource.cpp
@@ -483,6 +483,18 @@ bool RadarDataSource::loadData() {
   if (!initWithDummy(file_divs[1], file_amps[1]))
     return false;
   */
+  for(int i = 0; i < 2; i++)
+  {
+      float amp = (i + 1) * 64;
+      for(int j = 0; j < BEARINGS_PER_CYCLE; j++)
+      {
+          file_divs[i][j] = 1.0;
+          for(int k = 0; k < PELENG_SIZE; k++)
+          {
+              file_amps[i][(j * PELENG_SIZE) + k] = amp;
+          }
+      }
+  }
 
   return true;
 }
